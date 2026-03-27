15bebd2602218b32e989d9cb5a8c3d070c88c3db
rtrevizo18 <ricardotrevizo18@gmail.com>
2026-03-27 10:44:34 -0500

    feat: Add finder for thru SERP API

---
+++ src/people/finder.py
@@ -0,0 +1,53 @@
+import requests
+import dotenv
+import os
+from playwright.sync_api import sync_playwright
+import json
+from bs4 import BeautifulSoup
+
+dotenv.load_dotenv()
+
+SERP_API_KEY = os.getenv("SERP_API_KEY")
+
+def google_search(q):
+  url = "https://serpapi.com/search.json"
+
+  params = {
+    "engine": "google",
+    "q": q,
+    "location": "United States",
+    "google_domain": "google.com",
+    "hl": "en",
+    "gl": "us",
+    "api_key" : SERP_API_KEY
+  }
+
+  response = requests.get(url, params=params)
+
+  data = response.json()
+
+  return json.dumps(data)
+
+
+def bruh(query):
+  url = "https://www.google.com/search"
+
+  params = {
+    "q": query,
+  }
+
+  headers = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7)"
+  }
+
+  res = requests.get(url, params=params, headers=headers)
+
+  soup = BeautifulSoup(res.text, "html.parser")
+
+  for a in soup.select("a"):
+    link = a.get("href")
+    if link and "linkedin.com/in" in link:
+      print(link)
+
+if __name__ == '__main__':
+  bruh('site:linkedin.com/in "software engineer" Houston')
